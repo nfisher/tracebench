185d4ab1e00d0c1259880c08d16d8f1e4e78de91
Nathan Fisher <nfisher@junctionbox.ca>
2021-01-03 17:44:53 -0500

    Remove conditional from setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,7 @@
 
 BAZEL_VERSION=`cat .bazelversion`
 
-if [ ! -f ~/bin/bazel ]; then
 curl -fsSLO https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
 chmod 755 bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
 ./bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh --user
-else
-  echo "Bazel already present"
-fi
-
 ~/bin/bazel version
